6dc700d3542d0bd65c148d7d6a8fa4a5c042e070
Andrii KARPUS <karpus.andrii@proton.me>
2023-01-31 11:34:53 +0100

    Start working on the Rebelle

---
+++ src/main/java/org/sid/services/ChasseurServiceImpl.java
@@ -0,0 +1,57 @@
+package org.sid.services;
+
+import org.sid.dao.ChasseurRepository;
+import org.sid.entities.Chasseur;
+import org.sid.entities.Pilote;
+import org.sid.enums.EtatChasseur;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChasseurServiceImpl implements ChasseurService {
+
+    private final ChasseurRepository chasseurRepository;
+
+    public ChasseurServiceImpl(ChasseurRepository chasseurRepository) {
+	this.chasseurRepository = chasseurRepository;
+    }
+
+    @Override
+	public List<Chasseur> getChasseurs() {
+	return chasseurRepository.findAll();
+    }
+
+    @Override
+	public void addNewChasseur(Chasseur chasseur) {
+	Optional<Chasseur> chasseurOptional = chasseurRepository.findChasseurByName(chasseur.getName());
+	if (chasseurOptional.isPresent()) {
+	    throw new IllegalStateException("Ship is already in the database");
+	}
+	chasseurRepository.save(chasseur);
+    }
+
+    @Override
+	public void deleteChasseur(Long chasseurId) {
+	if (chasseurRepository.findById(chasseurId).isEmpty()) {
+	    throw new IllegalStateException("No ship with this id found");
+	}
+	chasseurRepository.deleteById(chasseurId);
+    }
+
+    @Override
+	@Transactional
+    public void updateChasseur(Long chasseurId, EtatChasseur etatChasseur, Pilote pilote) {
+	Chasseur chasseur = chasseurRepository.findById(chasseurId)
+		.orElseThrow(() -> new IllegalStateException("No ship with this id found"));
+
+	if (etatChasseur != null && chasseur.getEtatChasseur() != etatChasseur) {
+	    chasseur.setEtatChasseur(etatChasseur);
+	}
+	if (pilote != null && chasseur.getPilote().getId != pilote.getId) {
+	    chasseur.setPilote(pilote);
+	}
+    }
+}
--- src/main/java/org/sid/web/ChasseurController.java
+++ src/main/java/org/sid/web/ChasseurController.java
@@ -1,20 +1,12 @@
 package org.sid.web;
 
-import java.util.List;
-
 import org.sid.entities.Chasseur;
 import org.sid.entities.Pilote;
 import org.sid.enums.EtatChasseur;
 import org.sid.services.ChasseurService;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/chasseur")
